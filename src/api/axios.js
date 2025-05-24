8324f6f13135724de147f8ead2b4a5de8c62f801
Allan <allan.kretzmann@exati.com.br>
2025-05-24 15:33:29 -0300

    deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? 'https://59d9-2804-14c-8181-82ba-d0a-200f-9835-e021.ngrok-free.app' : 'http://localhost:3000',
+  baseURL: process.env.NODE_ENV === "production" ? 'https://03e2-2804-14c-8181-82ba-d0a-200f-9835-e021.ngrok-free.app' : 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
