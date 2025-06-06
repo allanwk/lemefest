2a1048b2b06d46b3ec3b093dbfa9a21e5af1809e
Allan <allan.kretzmann@exati.com.br>
2025-06-06 20:58:15 -0300

    fixa URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://leme.ngrok.app' : 'http://192.168.0.200:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-fetch('https://gist.githubusercontent.com/allanwk/87bd602da06208026c25dfe3309fabe9/raw/url.json' + '?t=' + Date.now())
-  .then(response => response.json())
-  .then(data => {
-    if (process.env.NODE_ENV === "production") {
-      api.defaults.baseURL = data.prod;
-    } else {
-      api.defaults.baseURL = data.dev;
-    }
-  });
+// fetch('https://gist.githubusercontent.com/allanwk/87bd602da06208026c25dfe3309fabe9/raw/url.json' + '?t=' + Date.now())
+//   .then(response => response.json())
+//   .then(data => {
+//     if (process.env.NODE_ENV === "production") {
+//       api.defaults.baseURL = data.prod;
+//     } else {
+//       api.defaults.baseURL = data.dev;
+//     }
+//   });
 
 api.interceptors.request.use(config => {
   config.headers['X-User-Uuid'] = localStorage.getItem('uuid_usuario');
